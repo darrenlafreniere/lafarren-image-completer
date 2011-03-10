4e99379f2a600280954e372b8b3c9115da19d098
David Doria <daviddoria@gmail.com>
2011-03-10 07:22:11 -0500

    ENH: guarded Poisson patch type

--- src/priority-bp/impl/Compositor.cpp
+++ src/priority-bp/impl/Compositor.cpp
@@ -1,19 +1,19 @@
 //
 // Copyright 2010, Darren Lafreniere
 // <http://www.lafarren.com/image-completer/>
-// 
+//
 // This file is part of lafarren.com's Image Completer.
-// 
+//
 // Image Completer is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Image Completer is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Image Completer, named License.txt. If not, see
 // <http://www.gnu.org/licenses/>.
@@ -64,13 +64,17 @@ Compositor* CompositorFactory::Create(CompositorPatchType patchType, CompositorP
 		patchimageSourceFactory = new PatchTypeNormal::Factory;
 		break;
 	case CompositorPatchTypePoisson:
+#ifdef USE_POISSON
 		patchimageSourceFactory = new PatchTypePoisson::Factory;
+#else
+        #pragma message("Not built with Poisson blending enabled!")
+#endif
 		break;
 	case CompositorPatchTypeDebugOrder:
 		patchimageSourceFactory = new PatchTypeDebugPatchOrder::Factory;
 		break;
 	}
-	
+
 	switch (patchBlender)
 	{
 	case CompositorPatchBlenderPriority:
